1dee4ff3a18a4b3fe565ebe4acf0f5c541ef03aa
Nicolas VENTER <thedeal78@hotmail.fr>
2025-07-23 15:47:08 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import PWABadge from "./PWABadge";
+
+export const App = () => {
+	return (
+		<div>
+			Hello World
+			<PWABadge />
+		</div>
+	);
+};
---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'clever-music-player',
+      short_name: 'clever-music-player',
+      description: 'Music player that consider skip in the choice of random music',
+      theme_color: '#27282C',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
