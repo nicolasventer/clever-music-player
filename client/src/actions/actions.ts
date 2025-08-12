e1d0cda889f3eb10b5db95dd273cadcf7f5946e9
Nicolas VENTER <thedeal78@hotmail.fr>
2025-08-12 14:49:34 +0200

    fix typo browser

--- client/src/actions/actions.ts
+++ client/src/actions/actions.ts
@@ -1,5 +1,5 @@
 import { app } from "@/actions/app";
-import { browser } from "@/actions/browseer";
+import { browser } from "@/actions/browser";
 import { dangerZone } from "@/actions/dangerZone";
 import { dashboard } from "@/actions/dashboard";
 import { player } from "@/actions/player";
